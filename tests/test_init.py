27384a47504854055d75c17f8e788c35998fd707
Matej Sekoranja <matej.sekoranja@gmail.com>
2020-12-31 01:06:56 +0100

    test

--- tests/test_init.py
+++ tests/test_init.py
@@ -8,7 +8,7 @@ from georss_client.exceptions import GeoRssException
 from tests import load_fixture
 from georss_emsc_csem_earthquakes_client import EMSCEarthquakesFeed
 
-HOME_COORDINATES = (46, 14)
+HOME_COORDINATES = (46.1, 14.2)
 
 class TestEMSCEarthquakesFeed(unittest.TestCase):
     """Test the EMSC CSEM Canada Earthquakes feed."""
@@ -25,32 +25,30 @@ class TestEMSCEarthquakesFeed(unittest.TestCase):
 
         feed = EMSCEarthquakesFeed(HOME_COORDINATES)
         assert repr(feed) == "<EMSCEarthquakesFeed(" \
-                             "home=(46, 14), url=https://www.emsc-csem.org/service/rss/rss.php?typ=emsc&min_lat=10&min_long=-30&max_long=65, " \
+                             "home=(46.1, 14.2), url=https://www.emsc-csem.org/service/rss/rss.php?typ=emsc&min_lat=10&min_long=-30&max_long=65, " \
                              "filter_radius=None, filter_min_magnitude=None)>"
         status, entries = feed.update()
         assert status == UPDATE_OK
         self.assertIsNotNone(entries)
         assert len(entries) == 2
 
-"""
         feed_entry = entries[0]
-        assert feed_entry.title == "Title 1"
-        assert feed_entry.external_id == "1234"
-        assert feed_entry.coordinates == (44.11, -66.23)
-        self.assertAlmostEqual(feed_entry.distance_to_home, 4272.4, 1)
+        assert feed_entry.title == "ML 2.2  PYRENEES"
+        assert feed_entry.external_id == "https://www.emsc-csem.org/Earthquake/earthquake.php?id=934374"
+        assert feed_entry.coordinates == (42.90, 0.20)
+        #self.assertAlmostEqual(feed_entry.distance_to_home, ???, 1)
         assert feed_entry.published \
-            == datetime.datetime(2018, 9, 29, 8, 30,
+            == datetime.datetime(2020, 12, 30, 21, 35,
                                  tzinfo=datetime.timezone.utc)
-        assert feed_entry.category == "Category 1"
-        assert feed_entry.magnitude == 4.5
-        assert feed_entry.attribution == "Natural Resources Canada"
-        assert repr(feed_entry) == "<NaturalResourcesCanadaEarthquakes" \
-                                   "FeedEntry(id=1234)>"
+        assert feed_entry.magnitude == 2.2
+        #assert feed_entry.attribution == None
+        assert repr(feed_entry) == "<EMSCEarthquakesFeedEntry(id=https://www.emsc-csem.org/Earthquake/earthquake.php?id=934374, " \ 
+                                   "title=ML 2.2  PYRENEES, link=https://www.emsc-csem.org/Earthquake/earthquake.php?id=934374, " \
+                                    "geometry=<Point(latitude=42.9, longitude=0.2)>, time=2020-12-30 21:14:43 UTC, depth=2, magnitude=2.2)>"
 
         feed_entry = entries[1]
-        assert feed_entry.title == "Title 2"
+        assert feed_entry.title == "ML 2.8  CRETE, GREECE"
         self.assertIsNone(feed_entry.published)
-"""
 
     @mock.patch("requests.Request")
     @mock.patch("requests.Session")
@@ -62,10 +60,10 @@ class TestEMSCEarthquakesFeed(unittest.TestCase):
             .return_value.text = \
             load_fixture('emsc_csem_earthquakes.xml')
 
-        feed = EMSCEarthquakesFeed(HOME_COORDINATES, filter_minimum_magnitude=4.0)
+        feed = EMSCEarthquakesFeed(HOME_COORDINATES, filter_minimum_magnitude=2.5)
         assert repr(feed) == "<EMSCEarthquakesFeed(" \
-                             "home=(46, 14), url=https://www.emsc-csem.org/service/rss/rss.php?typ=emsc&min_lat=10&min_long=-30&max_long=65, " \
-                             "filter_radius=None, filter_min_magnitude=4.0)>"
+                             "home=(46.1, 14.2), url=https://www.emsc-csem.org/service/rss/rss.php?typ=emsc&min_lat=10&min_long=-30&max_long=65, " \
+                             "filter_radius=None, filter_min_magnitude=2.5)>"
         status, entries = feed.update()
         assert status == UPDATE_OK
         self.assertIsNotNone(entries)
